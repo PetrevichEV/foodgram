b056ac05df49f037a18406328a9f1c78c1ae6325
Petrevich Evgenii <petrevich@yandex.ru>
2025-01-31 10:42:18 +0500

    1 31 01 25

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-7wvme3n)g8n3&r)ve&2frw7x1v88f38em^ua$g+9vw^8=ryjbg
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 5000
 
