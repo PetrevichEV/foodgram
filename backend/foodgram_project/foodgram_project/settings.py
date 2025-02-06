ef3fecdd3cd6bd2ef3e71a7d58b366c0d6653bc8
Petrevich Evgenii <petrevich@yandex.ru>
2025-02-06 17:11:02 +0500

    17 06 02 25

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -134,7 +134,6 @@ DJOSER = {
         'user': 'api.serializers.UserSerializer',
         'current_user': 'api.serializers.UserSerializer',
         'user_create': 'api.serializers.UserSerializer',
-        'token_create': 'djoser.serializers.TokenCreateSerializer',
     },
     'PERMISSIONS': {
         'user': ['rest_framework.permissions.IsAuthenticated'],
